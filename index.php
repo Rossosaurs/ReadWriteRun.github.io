ab7e79bc9c1fc6669a14fa8000c264ac445e3f3e
Ross Rawlings <Ross.Rawlings01@gmail.com>
2017-05-22 09:43:49 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Kilo.ioe</title>
+ 
+   <link rel="stylesheet" href="css/custom.css">
+ 
+	    <style>
+
+		#if-location {
+		position: absolute; left: 100px; top:200px;
+		width:1100px;
+		height:950px;
+	}
+	
+</style>
+
+
+</head>
+
+
+
+<body>
+
+<div class="nav" id="my nav">
+	<a href="http://10.183.1.11:8080"  >port 8080</a> &nbsp;
+	<a href="http://10.183.1.11:3000" >port 3000</a> &nbsp;
+	<a href= "/app" >App</a> &nbsp;
+
+	<a href="contact.html" >Contact</a>&nbsp;
+	<a href="/html"   >Html</a>&nbsp;
+	<a href="/downloads"  >Downloads </a>&nbsp;
+	<a href="/logs" >Logs</a>
+</div>
+
+
+<br>
+<br>
+<a href = "https://sites.google.com/a/tritechsc.org/rossr"  target = "_blank"><img src = "img/google.png" width = "75px" ></a>
+<a href="https://rossosaurs.github.io/"target = "if" ><img src = "img/dinosaur.png" width = "75px" ></a>
+<a href = "/php/formphp"  target = "_blank"><img src = "img/php.jpg" height="75px" ></a>
+<a href = "/qnb"  target = "_blank"><img src = "img/phpma.png" height="75px" ></a>
+<a href = "/javascript"  target = "_blank"><img src = "img/js.jpg" height="75px" ></a>
+
+
+
+
+<br>
+<br>
+<br>
+<br>
+
+<img src = "img/hackedcomputer.jpg" width = "500px">
+
+</body>
+</html>
